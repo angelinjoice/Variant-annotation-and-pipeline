52363bc7e8196d586a1896b03b793f9dcffb574a
angelinjoice <angelinjoice1@gmail.com>
2026-09-03 05:27:49 +0000

    first step completed

--- Step1.py
+++ Step1.py
@@ -1,1 +1,16 @@
+#Extracting vcf  file
+from cyvcf2 import VCF, Writer
 
+input_vcf = "clinvar.vcf.gz"
+output_vcf = "cftr_clinvar.vcf"
+REGION = "7:117465784-117682387"
+
+vcf = VCF(input_vcf)
+out_vcf = Writer(output_vcf, vcf)
+
+for variant in vcf(REGION):
+    out_vcf.write_record(variant)
+
+out_vcf.close()
+vcf.close()
+print("Extracted cftr_clinvar.vcf successfully")
